e577dc9e6157d7ce16768800eb126de35ad33a53
sergio-echigo <sergio-echigo.wipe764@passmail.net>
2026-03-25 21:33:00 -0300

    improve migration spec. and add migration file sample

--- migrator/migrator-core/src/specs.clj
+++ migrator/migrator-core/src/specs.clj
@@ -2,4 +2,24 @@
   (:require [clojure.spec.alpha :as s]))
 
 ;; TODO: define database access settings spec
-;; TODO: define the migration specification
+(defn- unix? [timestamp]
+  (try
+    (Instant/ofEpochMilli timestamp)
+    (catch Exception _
+      false)))
+
+;; Migration specification
+(s/def :statement/apply #(not (clojure.string/blank? %)))
+(s/def :statement/rollback #(not (clojure.string/blank? %)))
+
+(s/def :statement/key #(not (clojure.string/blank? %)))
+(s/def :statement/value (s/keys :req-un [:statement/apply]
+                                :opt-un [:statement/rollback]))
+
+(s/def :migration/id #(uuid? (parse-uuid %)))
+(s/def :migration/timestamp (s/and int? pos? unix?))
+(s/def :migration/description string?)
+(s/def :migration/statements (s/map-of :statement/key :statement/value))
+
+(s/def :migration/migration (s/keys :req-un [:migration/id :migration/timestamp :migration/statements]
+                                    :req-opt [:migration/description]))
